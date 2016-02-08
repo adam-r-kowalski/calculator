91391c8e3c3b27c5536e76b305e3571f0610c220
Adam Kowalski <adam.kowalski.work@gmail.com>
2016-02-08 11:46:00 -0800

    add a readme file

---
+++ README.md
@@ -0,0 +1,26 @@
+# Calculator Demo
+- - -
+
+## Quick Start
+
+1. Clone the repository
+```bash
+git clone https://github.com/adam-r-kowalski/calculator.git
+```
+
+2. Start a repl
+```bash
+lein repl
+```
+
+3. Start figwheel
+```cljs
+(start)
+```
+
+4. Start the clojurescript repl
+```cljs
+(cljs)
+```
+
+5. Point your browser to: ```http://localhost:3449```
--- src/calculator/style.cljs
+++ src/calculator/style.cljs
@@ -1,7 +1,5 @@
 (ns calculator.style)
 
-(into {} [{:a 1} {:b 2} {:c 3 :b 3}])
-
 (def column
   {:display "flex"
    :flex-direction "column"})
@@ -17,36 +15,29 @@
    :align-items "flex-end"})
 
 (def container-styles
-  (into {} [column
-            {:height "100%"
-             :font-family "'Advent Pro', sans-serif"}]))
+  (merge column
+         {:height "100%" :font-family "'Advent Pro', sans-serif"}))
 
 (def display-styles
-  (into {} [bottom-right
-            {:background-color "black"
-             :color "white"
-             :padding "0 25px 15px 0"
-             :font-size "72px"
-             :flex 1}]))
+  (merge bottom-right
+         {:background-color "black"
+          :color "white"
+          :padding "0 25px 15px 0"
+          :font-size "72px"
+          :flex 1}))
 
 (def calculator-styles
-  (into {} [column
-            {:flex 3
-             :background-color "white"}]))
+  (merge column {:flex 3 :background-color "white"}))
 
-(def row-styles
-  {:flex 1
-   :display "flex"})
+(def row-styles {:flex 1 :display "flex"})
 
 (defn button-styles
-  ([] (into {}[center
-               {:flex 1
-                 :background-color "lightgray"
-                 :font-size "32px"
-                 :border-top "1px solid #333"
-                 :border-right "1px solid #333"
-                 :cursor "pointer"}]))
-  ([c] (merge (button-styles)
-              {:background-color c}))
-  ([c f] (merge (button-styles c)
-                {:flex f})))
+  ([] (merge center
+             {:flex 1
+              :background-color "lightgray"
+              :font-size "32px"
+              :border-top "1px solid #333"
+              :border-right "1px solid #333"
+              :cursor "pointer"}))
+  ([c] (merge (button-styles) {:background-color c}))
+  ([c f] (merge (button-styles c) {:flex f})))
